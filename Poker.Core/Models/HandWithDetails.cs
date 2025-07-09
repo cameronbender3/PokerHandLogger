7c6859039889974cd6c82d2f910e3fdf0fc613d9
cameronbender3 <bendercameron33@gmail.com>
2025-07-09 13:30:23 -0400

    Update HandWithDetails.cs

--- Poker.Core/Models/HandWithDetails.cs
+++ Poker.Core/Models/HandWithDetails.cs
@@ -6,7 +6,7 @@ public class HandWithDetails
 {
     public Hand? Hand { get; set; }
     public List<Player>? Players { get; set; } 
-    public List<Action>? Actions { get; set; } 
+    public List<Action> Actions { get; set; } = new List<Action>();
     public List<Card>? Cards { get; set; }
 }
 
